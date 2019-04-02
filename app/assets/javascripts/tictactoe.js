f9d625bc6b67b435f456c0496a6e3ff6c32d050a
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-02 15:47:46 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,24 @@
 // Code your JavaScript / jQuery solution here
+var turn = 0
+var winningIndices = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 7], [2, 4, 6]
+]
+
+var player = function() {
+  return (turn % 2 == 0 ? "X" : "O")
+}
+
+var updateState = function(tdEl) {
+  $(tdEl).text(player())
+}
+
+var setMessage = function(string) {
+  $('#message').text(string)
+}
+
+var checkWinner = function() {
+  let squares = $('td')
+  
+}
